dccb4f90a3cd9ba152dccefc58f1b5c5b202489b
Moon Antonio <pinchodev91@gmail.com>
2017-03-28 16:49:27 +0200

    Add MController.cs

---
+++ Scripts/MController.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// MController.cs (28/03/2017)													\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Controlador general del personaje							\\
+// Fecha Mod:		28/03/2017													\\
+// Ultima Mod:		Version inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace Pendulum.Controller
+{
+	/// <summary>
+	/// <para>Controlador general del personaje</para>
+	/// </summary>
+	[AddComponentMenu("Pendulum/Controller/MController")]
+	public class MController : MonoBehaviour 
+	{
+
+	}
+}
